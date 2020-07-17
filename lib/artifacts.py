cc9463af86707298f000ff4882cf1fb2f7d94e2e
Zjonn <zjonnn@gmail.com>
2020-07-18 00:10:28 +0200

    Moved requesting for the artifacts relative_paths from Build to Artifacts
    Don't need the data on Artifats class initialization,
    so it will be lazy resource, downloaded when will be needed

--- lib/artifacts.py
+++ lib/artifacts.py
@@ -1,12 +1,24 @@
 import typing
 
-from .json_requests import get
+from .jenkins_requests import get_node
 
 
 class Artifacts:
     def __init__(self, desc: typing.Union[typing.Dict]):
-        self.url = desc['url']
-        self.relative_paths = desc['relative_paths']
+        self.url = desc["url"]
+
+        self._relative_paths = None
+        self._paths = None
+
+    @property
+    def relative_paths(self):
+        if self._relative_paths:
+            return self._relative_paths
+
+        resp = get_node(self.url, "artifacts", subnodes="relativePath")
+        self._relative_paths = list(map(lambda r_path: r_path["relativePath"], resp))
+
+        return self._relative_paths
 
     def __str__(self):
-        return f'Artifacts from {self.url}'
+        return f"Artifacts from {self.url}"
