386f541090c525760b83ce27ea352d562bbe3947
Zjonn <zjonnn@gmail.com>
2020-10-14 00:36:43 +0200

    Correct TestBuildUrl unittests

--- tests/test_json_requests.py
+++ tests/test_json_requests.py
@@ -5,27 +5,23 @@ from lib.json_requests import build_url
 
 class TestBuildUrl(unittest.TestCase):
     def test_empty(self):
-        self.assertEqual(build_url(''), (''))
+        self.assertEqual(build_url(""), (""))
 
     def test_list_no_backslash(self):
-        self.assertEqual(build_url(['', '']), '/')
+        self.assertEqual(build_url(["a", "b"]), "a/b")
 
     def test_list_one_backslash(self):
-        self.assertEqual(build_url(['/', '']), '/')
+        self.assertEqual(build_url(["a/", "b"]), "a/b")
 
     def test_list_two_backslash(self):
-        self.assertEqual(build_url(['/', '/']), '/')
+        self.assertEqual(build_url(["a/", "/b"]), "a/b")
 
     def test_both_backslash(self):
-        self.assertEqual(build_url('/', '/'), '/')
+        self.assertEqual(build_url("a/", "/b"), "a/b")
 
     def test_one_backslash(self):
-        self.assertEqual(build_url('/', ''), '/')
-        self.assertEqual(build_url('', '/'), '/')
+        self.assertEqual(build_url("a/", "b"), "a/b")
+        self.assertEqual(build_url("a", "/b"), "a/b")
 
     def test_no_backslash(self):
-        self.assertEqual(build_url('', ''), '/')
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(build_url("a", "b"), "a/b")
